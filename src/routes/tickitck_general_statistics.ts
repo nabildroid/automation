f5ddb010711aee570bc61997257bd2bf582c8ed7
nabildroid <pni20156789@gmail.com>
2021-09-29 19:13:38 +0100

    convert statistics output to support google data studio

--- src/routes/tickitck_general_statistics.ts
+++ src/routes/tickitck_general_statistics.ts
@@ -11,6 +11,36 @@ export default class TicktickGeneralStatistics implements IRoute {
 	}
 
 	async handler(req: Request, res: Response) {
-		res.send(JSON.stringify(await this.app.ticktick.getGeneralStatistis()));
+		const {
+			level,
+			score,
+			todayCompleted,
+			totalCompleted,
+			yesterdayCompleted,
+			scoreByDay,
+			taskByDay,
+		} = await this.app.ticktick.getGeneralStatistis();
+
+		const response = {
+			level,
+			score,
+			todayCompleted,
+			totalCompleted,
+			yesterdayCompleted,
+			daysRow: Object.keys(scoreByDay).map(formatTime),
+			scoreByDay:Object.values(scoreByDay),
+			taskByDay:Object.values(taskByDay).map(v=>v.completeCount),
+		};
+
+        res.send(JSON.stringify(response));
 	}
+}
+
+/** convert from 20210908 to 2021 09 08 */
+function formatTime(str: string) {
+	const year = str.slice(0, 4);
+	const month = str.slice(4, 6);
+	const day = str.slice(6);
+
+	return new Date([year, month, day].join(" "));
 }
