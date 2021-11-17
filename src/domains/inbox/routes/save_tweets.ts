0f0aa0644f69a68fbb65a3544a065fa02f894450
nabildroid <pni20156789@gmail.com>
2021-11-17 21:29:17 +0100

    create save_tweets route

---
+++ src/domains/inbox/routes/save_tweets.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { IRoute } from "../../../core/service";
+import Notion from "../repositories/notion";
+import Twitter from "../repositories/twitter";
+
+export default class SaveTweets implements IRoute {
+  notion: Notion;
+  twitter: Twitter;
+
+  constructor(notion: Notion, twitter: Twitter) {
+    this.notion = notion;
+    this.twitter = twitter;
+  }
+
+  async handler(req: Request, res: Response) {
+    const id = req.params.id as string;
+
+    const tweets = await this.twitter.thread(id);
+
+    const head = tweets[0]!;
+
+
+    
+
+    await this.notion.addToInbox({
+      title:head.text.split("\n")[0],
+      body:Notion.TweetsTemplate(tweets),
+      done:false,
+      tags:[]
+    },{
+      context:["twitter"],
+      source:head.link
+    })
+
+    res.send(head.text.split("\n")[0]);
+  }
+}
