5ea878e96d8f9cd718cdee9c3b36137875810371
nabildroid <pni20156789@gmail.com>
2022-01-23 20:23:02 +0100

    code formatting

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
-import Express from "express";
-import App from "./app";
-import admin from "firebase-admin";
-import { EventEmitter } from "events";
 import fs from "fs";
-import Winston from "winston"
-
+import Express from "express";
+import admin from "firebase-admin";
+import Winston from "winston";
 import { LoggingWinston } from "@google-cloud/logging-winston";
+import { EventEmitter } from "events";
+
+import App from "./app";
 import { needAuthorization } from "./guard";
 
-const loggingWinston = new LoggingWinston({
-  projectId:"supernabil-86c2b",
-});
-
-
-const logger = Winston.createLogger({
-  defaultMeta:{
-    project:"supernabil heroku",
-  },
-  transports: [
-    new Winston.transports.Console(),
-    loggingWinston,
-  ],
-});
-
-
-
-
 require("dotenv").config();
+
 const port = process.env.PORT || 8080;
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT || "{}");
 const authorization = process.env.SUPERNABIL_AUTHORIZATION;
+
+const loggingWinston = new LoggingWinston({
+  projectId: "supernabil-86c2b",
+});
+
+const logger = Winston.createLogger({
+  defaultMeta: {
+    project: "supernabil heroku",
+  },
+  transports: [new Winston.transports.Console(), loggingWinston],
+});
 
 const server = Express();
 server.use(Express.json());
@@ -44,32 +38,28 @@ server.use((req, res, next) => {
     next();
   } else {
     res.redirect("https://laknabil.notion.site");
+    logger.warning("unauthorized request to " + req.path);
   }
 });
 
-const app = new App(server,logger);
-
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-
-fs.writeFileSync(".service_account.js",JSON.stringify(serviceAccount));
-process.env.GOOGLE_APPLICATION_CREDENTIALS  = ".service_account.js";
-
-
-
-
+fs.writeFileSync(".service_account.js", JSON.stringify(serviceAccount));
+process.env.GOOGLE_APPLICATION_CREDENTIALS = ".service_account.js";
 
 const storage = admin.storage();
 const bucket = storage.bucket(process.env.BUCKET);
 
 export const bus = new EventEmitter();
 
+const app = new App(server, logger);
 
 const timer = logger.startTimer();
-server.listen(port, async () => {
+
+app.init(admin.firestore(), bucket).then(() => {
+  timer.done({ message: "Init App" });
+});
+
+server.listen(port, () => {
   console.log(`listening on port ${port} `);
-
-  await app.init(admin.firestore(), bucket);
-
-  timer.done({message:"Init App"});
 });
